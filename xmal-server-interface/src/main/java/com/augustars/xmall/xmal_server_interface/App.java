a87633425d741a67b476eb8ab6cd30d1c19105fb
Administrator <Administrator@192.168.0.129>
2018-08-23 19:51:16 +0800

    First Commit xmall/xmall-server-interface/xmall-server

---
+++ xmal-server-interface/src/main/java/com/augustars/xmall/xmal_server_interface/App.java
@@ -0,0 +1,13 @@
+package com.augustars.xmall.xmal_server_interface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
